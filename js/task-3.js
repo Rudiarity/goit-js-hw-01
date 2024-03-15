d1865067d18ad21eb2c130737af4c2446fad141d
Kyrylo_Fedechko <rudiari300@gmail.com>
2024-03-15 23:22:38 +0200

    update task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,12 @@
 function getElementWidth(content, padding, border) {
-    const borderBox = border * 4;
-    return   content + padding + borderBox;
+    const sizeContent = Number.parseFloat(content);
+    const sizePadding = Number.parseFloat(padding);
+    const sizeBorder = Number.parseFloat(border);
+    const borderBox = sizeBorder * 4;
+    const totalSize = sizeContent + sizePadding + borderBox;
+    return   totalSize;
     
 }
 console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 106
+console.log(getElementWidth("200px", "0px", "0px")); // 200
